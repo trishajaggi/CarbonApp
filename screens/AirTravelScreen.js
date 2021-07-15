37c7e37219af4bb8ae919ea2b1cbb1119847979c
trishajaggi <trishajaggi@gmail.com>
2021-07-15 11:29:34 -0700

    7/15 functionality

--- screens/AirTravelScreen.js
+++ screens/AirTravelScreen.js
@@ -5,22 +5,25 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button } from 'react-native';
 
-export default function AirTravelScreen ({navigation}) {
-    const [miles, setMiles] = useState(0);
+export default function AirTravelScreen ({ navigation }) {
 
-
+    const [miles, setMiles] = useState('');
     let placeholder = 'Enter average miles traveled per month';
+
+
     return (
         <View>
             <TextInput style = {{height: 60, fontSize: 20}}
-                placeholder = {placeholder}
+                placeholder = { placeholder }
                 value = { miles }
                 onChangeText = { setMiles }
             />
 
             <Button style = {{ flex: 1, alignItems: 'center', justifyContent: 'flex-end' }}
                 title = "Done"
-                onPress = {() => navigation.navigate('Home')}
+                onPress = { () => navigation.navigate( 'Home', {
+                    airReturn: miles,
+                    })}
             />
         </View>
     );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,45 +1,79 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-const-assign */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 
-import React, { useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useCallback, useState } from 'react';
 import { View, StyleSheet, Button, TextInput } from 'react-native';
+import VehicleScreen from './VehicleScreen';
+
+export default function HomeScreen ({navigation, route}) {
+
+    let [vehicleReturn, setVehicleReturn] = useState('');
+    let [utilityReturn, setUtilityReturn] = useState('');
+    let [airReturn, setAirReturn] = useState('');
+    useFocusEffect(
+        useCallback(() =>{
+            if ( route.params ){
+                setVehicleReturn( route.params.vehicleReturn );
+            }
+        }, [route])
+    );
+
+    useFocusEffect(
+        useCallback(() =>{
+            if ( route.params ) {
+                setAirReturn( route.params.airReturn );
+            }
+        }, [route])
+    );
+
+    useFocusEffect(
+        useCallback(() =>{
+            if ( route.params ) {
+                setUtilityReturn( route.params.utilityReturn );
+                //console.log(utilityReturn);
+            }
+        }, [route])
+    );
 
 
-export default function HomeScreen ({navigation}) {
-    const [vehicleReturn, setVehicleReturn] = useState('');
-    const [utilityReturn, setUtilityReturn] = useState('');
-    const [airReturn, setAirReturn] = useState('');
     return (
+
         <View style = { styles.home }>
 
             <TextInput style = { styles.border }
-
+                placeholder = { 'input data at vehicle screen' }
+                value = { vehicleReturn }
+                onChangeText = { setVehicleReturn }
             />
 
             <Button style = {{fontSize: 30}}
                 title = "Vehicle"
                 onPress = {() => navigation.navigate('Vehicle')}
-
             />
 
             <TextInput style = { styles.border }
-                placeholder = {''}
+                placeholder = { 'input data at utility screen' }
+                value = { utilityReturn }
+                onChangeText = { setUtilityReturn }
             />
 
             <Button style = {{fontSize: 30}}
                 title = "Utility"
                 onPress = {() => navigation.navigate('Utility')}
-
             />
 
             <TextInput style = { styles.border }
-                placeholder = {''}
+                placeholder = { 'input data at air travel screen' }
+                value = { airReturn }
+                onChangeText = { setAirReturn }
             />
 
             <Button style = {{fontSize: 30}}
                 title = "Air Travel"
                 onPress = {() => navigation.navigate('Air Travel')}
-
             />
 
             <View style = {{flex: 1, alignItems: 'center', justifyContent: 'flex-end'}}>
@@ -69,10 +103,10 @@ const styles = StyleSheet.create({
 
     border: {
         flex: 1,
-        backgroundColor: 'blue',
+        backgroundColor: 'white',
         width: 100,
         height: 10,
-        fontSize: 30,
+        fontSize: 10,
         alignItems: 'center',
         borderColor: 'black',
     },
